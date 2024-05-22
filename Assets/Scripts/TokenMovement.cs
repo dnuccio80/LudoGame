e7c64961a1314ca9d312682541d452d8b2ea3e6a
dnuccio80 <nuccio227@gmail.com>
2024-05-21 22:17:48 -0300

    deleted Anims , added circles to tokens

--- Assets/Scripts/TokenMovement.cs
+++ Assets/Scripts/TokenMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class Player : MonoBehaviour
+public class TokenMovement : MonoBehaviour
 {
 
     [SerializeField] private Transform[] ways;
